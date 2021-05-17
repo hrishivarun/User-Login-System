b49f36d0702436d87e25aa957ab545e726eef308
varunhrishi <varun.hrishi.18@gmail.com>
2021-05-18 01:42:32 +0530

    Confused with file paths

---
+++ Login_files/createnew.php
@@ -0,0 +1,12 @@
+<?php 
+include_once 'db_connect.php';
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta name="viewport", content="width=device-width, initial-scale=1.0">
+        <title>Create new account</title>
+        <link rel="stylesheet" href="CSS.css">
+    </head>
+</html>
